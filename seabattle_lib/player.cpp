818ac6835673f601ba87d3d8aaa56afd83732cac
Даниил <rebrovdanil506@gmail.com>
2026-09-23 22:23:57 +0300

    feat: реализован класс Player + тесты

--- seabattle_lib/player.cpp
+++ seabattle_lib/player.cpp
@@ -1,1 +1,22 @@
-#pragma once
+#include "player.h"
+#include <stdexcept>
+#include <utility>
+
+namespace seabattle {
+
+    Player::Player(std::string name)
+        : name_(std::move(name)) {
+        if (name_.empty()) {
+            throw std::invalid_argument("Player name cannot be empty");
+        }
+    }
+
+    ShotResult Player::make_shot(Player& opponent, const Position& pos) {
+        return opponent.field_.receive_shot(pos);
+    }
+
+    bool Player::lost() const noexcept {
+        return field_.all_ships_dead();
+    }
+
+} 
